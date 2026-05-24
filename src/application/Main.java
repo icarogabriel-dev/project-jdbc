0689b03429eb6b9bd88ea1e218a3da74ec523971
Icaro Gabriel <icarog584@gmail.com>
2026-05-24 17:25:29 -0300

    Creation of functions instantiateDepartment and instantiateSeller

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,8 +11,10 @@ public class Main {
     static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        Seller seller = sellerDao.findById(4);
 
+        System.out.println("=== TEST 1: Seller FindById ===");
+
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
