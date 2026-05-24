9b35f27ab873256d8edbdb0d794b6eb31cbf5392
Icaro Gabriel <icarog584@gmail.com>
2026-05-23 22:56:39 -0300

    Creation of the SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,9 @@ public class Main {
 
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(3000.0, new Date(), obj, "maria@gmail.com", 21, "Maria");
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
